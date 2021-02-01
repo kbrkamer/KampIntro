e84962138ad39dfe573e8c41176896645694debc
KÜBRA KAMER <kubrakamer.000@outlook.com>
2021-02-01 13:17:58 +0300

    4. GÜN SONU

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; } //Ürün Id
+        public int CategoryId { get; set; } // Hangi Kategori dahil olduğu 
+        public string ProductName { get; set; } //ürünün ismi
+        public double UnitPrice { get; set; } //Ürünün fiyatı
+        public int UnitsInStock { get; set; } //Ürünün Stok Adedi
+
+        //Crud operasyonlar
+    }
+}
