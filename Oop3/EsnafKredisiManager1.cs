0709982e83f38b1f3d7f992afaadedbc71478b5d
KÜBRA KAMER <kubrakamer.000@outlook.com>
2021-02-13 15:11:23 +0300

    5.GÜN

---
+++ Oop3/EsnafKredisiManager1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Oop3
+{
+    class EsnafKredisiManager : IKrediManager
+    {
+        public void BiseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Esnaf kredisi ödeme planı hesaplandı.");
+        }
+    }
+}
---
+++ Oop3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaceLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new DatabaseLoggerService(), new SmsLoggerService() };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, konutKrediManager };
+
+            // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
